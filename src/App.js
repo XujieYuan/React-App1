122f9c7a5106e0bfd0acd61cd03e9fcdb330ec84
Xujie Yuan <xyua0019@student.monash.edu>
2021-06-01 11:42:12 +0800

    setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine, chatEngine } from "react-chat-engine";
+
+import "./App.css";
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID=""
+            userName=""
+            userSecret=""
+        />
+    );
+}
+
+export default App;
