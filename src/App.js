c73a44f148c3fb4ae169d4d9658649c4d5995f64
Xujie Yuan <xyua0019@student.monash.edu>
2021-06-01 16:38:27 +0800

    login feature

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine } from "react-chat-engine";
 
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
 import "./App.css";
 
 const App = () => {
+    if (!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine
             height="100vh"
             projectID="be762530-d765-4949-92e9-ee87cd2d4c57"
-            userName="Faker"
-            userSecret="123123"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     );
